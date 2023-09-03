8e1f9782c17bfb35cdd5935fde8a29942d03a3dc
Laeti-dev <laetdi@gmail.com>
2023-09-03 18:19:00 +0200

    input handler

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Button from "../UI/Button";
 import Card from "../UI/Card";
@@ -6,16 +6,37 @@ import Card from "../UI/Card";
 import style from "./AddUser.module.css";
 
 const AddUser = () => {
+  const users = []
+  const [userName, setUserName] = useState("");
+  const [userAge, setUserAge] = useState("")
+
+  const userInputHandler = (input, value) => {
+    if (input === "Name") {
+      setUserName(value)
+    } else {
+      setUserAge(value)
+    }
+  };
+
+  const submitUserInputHandler = (event, data) => {
+    event.preventDefault();
+    if (userName.length !== 0 && userAge !== 0) {
+      users.push({"userName": userName, "userAge": userAge});
+      console.log(users)
+    } else {
+      console.log("missing info");
+    };
+  }
   return(
     <Card className={style.input}>
-      <form>
+      <form onSubmit={submitUserInputHandler}>
 
         <label htmlFor="Name">Name :</label>
-        <input type="text"/>
+        <input type="text" onChange={(event) => {userInputHandler("Name", event.target.value)}}/>
 
 
         <label htmlFor="Age">Age :</label>
-        <input type="number"/>
+        <input type="number" onChange={(event) => {userInputHandler("Age", event.target.value)}}/>
 
         <Button type="submit">
           Add user
