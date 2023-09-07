9fc292dbb13d6b8ce5c3a5beac854470742952c3
Laeti-dev <laetdi@gmail.com>
2023-09-07 07:37:05 +0200

    starting with portal for modals

--- src/components/UI/ErrorMessage.js
+++ src/components/UI/ErrorMessage.js
@@ -1,14 +1,17 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import Button from "./Button";
 
 import style from "./ErrorMessage.module.css";
 
-const ErrorMessage = (props) => {
+const Backdrop = props => {
+  return <div className={style.back} onClick={props.onCloseBtnHandler}/>;
+};
 
+const ModalOverlay = props => {
   return (
-    <div className={style.back} onClick={props.onCloseBtnHandler}>
-      <div className={style["pop-up"]}>
+    <div className={style["pop-up"]}>
         <div className={style.top}>
           <h1>{props.title}</h1>
         </div>
@@ -17,8 +20,27 @@ const ErrorMessage = (props) => {
           <Button onClick={props.onCloseBtnHandler}>Close</Button>
         </div>
       </div>
+  )
+};
 
-    </div>
+const ErrorMessage = (props) => {
+
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop
+          onClick={props.onCloseBtnHandler}/>,
+          document.getElementById("backdrop-root")
+        )}
+        {ReactDOM.createPortal(
+          <ModalOverlay
+            title={props.title}
+            message={props.message}
+            onCloseBtnHandler={props.onCloseBtnHandler}
+          />,
+          document.getElementById("overlay-root")
+        )}
+    </React.Fragment>
   )
 };
 
